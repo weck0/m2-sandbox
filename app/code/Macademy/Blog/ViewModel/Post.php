ecdca7346ac9d6d55d2f8264737cb2d911936fed
Florian Ceprika <f.ceprika@gmail.com>
2023-05-16 15:28:11 +0200

    detail view

--- app/code/Macademy/Blog/ViewModel/Post.php
+++ app/code/Macademy/Blog/ViewModel/Post.php
@@ -2,7 +2,10 @@
 
 namespace Macademy\Blog\ViewModel;
 
+use Macademy\Blog\Api\Data\PostInterface;
+use Macademy\Blog\Api\PostRepositoryInterface;
 use Macademy\Blog\Model\ResourceModel\Post\Collection;
+use Magento\Framework\App\RequestInterface;
 use Magento\Framework\View\Element\Block\ArgumentInterface;
 
 class Post implements ArgumentInterface
@@ -10,6 +13,8 @@ class Post implements ArgumentInterface
 
     public function __construct(
         private Collection $collection,
+        private PostRepositoryInterface $postRepository,
+        private RequestInterface $request,
     )
     {}
 
@@ -21,5 +26,11 @@ class Post implements ArgumentInterface
     public function getCount(): int
     {
         return $this->collection->count();
+    }
+
+    public function getDetail(): PostInterface
+    {
+        $id = (int) $this->request->getParam('id');
+        return $this->postRepository->getById($id);
     }
 }
