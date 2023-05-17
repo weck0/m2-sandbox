e2dadeccfa8acc8374353fb4c43fe01aa5fdf632
Florian Ceprika <f.ceprika@gmail.com>
2023-05-17 09:06:10 +0200

    adding event observer

--- app/code/Macademy/Blog/Controller/Post/Detail.php
+++ app/code/Macademy/Blog/Controller/Post/Detail.php
@@ -3,17 +3,25 @@
 namespace Macademy\Blog\Controller\Post;
 
 use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\Event\ManagerInterface as EventMangager;
 use Magento\Framework\View\Result\Page;
 use Magento\Framework\View\Result\PageFactory;
 
 class Detail implements HttpGetActionInterface
 {
     public function __construct(
-        private readonly PageFactory $pageFactory
+        private readonly PageFactory $pageFactory,
+        private EventMangager $eventManager,
+        private RequestInterface $request,
     ) {}
 
     public function execute(): Page
     {
+        $this->eventManager->dispatch('macademy_blog_post_detail_view', [
+            'request' => $this->request,
+        ]);
+
         return $this->pageFactory->create();
     }
 }
