6c9f1ddfb0cffb4578edc84088cd5366b8c6823a
Florian Ceprika <f.ceprika@gmail.com>
2023-05-17 09:32:42 +0200

    adding alternate post template

---
+++ app/code/Macademy/BlogExtra/Plugin/AlternatePostTemplate.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace Macademy\BlogExtra\Plugin;
+
+use Macademy\Blog\Controller\Post\Detail;
+use Magento\Framework\App\RequestInterface;
+
+class AlternatePostTemplate
+{
+
+    public function __construct(
+        private RequestInterface $request,
+    )
+    {
+    }
+
+    public function afterExecute(
+        Detail $subject,
+        $result,
+    ){
+        if($this->request->getParam('alternate')) {
+            $result->getLayout()
+                ->getBlock('blog.post.detail')
+                ->setTemplate('Macademy_BlogExtra::post/detail.phtml');
+        }
+        return $result;
+    }
+}
