91d3923250d8d70883cd592c9192c602baa10d7c
Florian Ceprika <f.ceprika@gmail.com>
2023-05-19 10:25:51 +0200

    refactor index controller

--- app/code/Blizzard/Warcraft/Controller/Index/Index.php
+++ app/code/Blizzard/Warcraft/Controller/Index/Index.php
@@ -1,50 +1,19 @@
 <?php
 namespace Blizzard\Warcraft\Controller\Index;
 
-use Magento\Framework\App\Action\Context;
-use Magento\Framework\App\Action\HttpGetActionInterface as HttpGetActionInterface;
-use Magento\Framework\App\Action\HttpPostActionInterface as HttpPostActionInterface;
+use Magento\Framework\App\Action\HttpGetActionInterface;
 use Magento\Framework\Controller\Result\JsonFactory;
 use Magento\Framework\Controller\ResultInterface;
 use Magento\Framework\View\Result\PageFactory;
 
-class Index implements HttpGetActionInterface, HttpPostActionInterface
+class Index implements HttpGetActionInterface
 {
-    /**
-     * @var Context
-     */
-    protected Context $context;
-
-    /**
-     * @var PageFactory
-     */
-    protected PageFactory $resultPageFactory;
-
-    /**
-     * @var JsonFactory
-     */
-    protected JsonFactory $resultJsonFactory;
-
-    /**
-     * @param Context $context
-     * @param PageFactory $resultPageFactory
-     * @param JsonFactory $resultJsonFactory
-     */
     public function __construct(
-        Context $context,
-        PageFactory $resultPageFactory,
-        JsonFactory $resultJsonFactory
+        private PageFactory $resultPageFactory,
+        private JsonFactory $resultJsonFactory
     ) {
-        $this->context = $context;
-        $this->resultPageFactory = $resultPageFactory;
-        $this->resultJsonFactory = $resultJsonFactory;
     }
 
-    /**
-     * Dispatch request
-     *
-     * @return ResultInterface
-     */
     public function execute(): ResultInterface
     {
         $result = $this->resultJsonFactory->create();
