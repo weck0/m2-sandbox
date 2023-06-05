6e465a205521583ce6d6f147c974720404052a3c
Florian Ceprika <f.ceprika@gmail.com>
2023-06-05 21:19:08 +0200

    refactor

--- app/code/Cargo/Promo/Helper/EmailHelper.php
+++ app/code/Cargo/Promo/Helper/EmailHelper.php
@@ -1,18 +1,42 @@
 <?php
+
 namespace Cargo\Promo\Helper;
 
-class EmailHelper
+use Magento\Framework\App\Helper\AbstractHelper;
+use Magento\Framework\Mail\Template\TransportBuilder;
+use Magento\Store\Model\StoreManagerInterface;
+use Magento\Framework\App\Helper\Context;
+
+class EmailHelper extends AbstractHelper
 {
-    // add necessary dependencies here
+    protected $transportBuilder;
+    protected $storeManager;
 
     public function __construct(
-        // add necessary dependencies here
+        Context $context,
+        TransportBuilder $transportBuilder,
+        StoreManagerInterface $storeManager
     ) {
-        // assign dependencies
+        $this->transportBuilder = $transportBuilder;
+        $this->storeManager = $storeManager;
+        parent::__construct($context);
     }
 
     public function sendEmail($email, $couponCode)
     {
-        // logic to send email to the given email address with the given coupon code
+        $templateVars = [
+            'store' => $this->storeManager->getStore(),
+            'coupon_code' => $couponCode
+        ];
+
+        $transport = $this->transportBuilder
+            ->setTemplateIdentifier('email_template') // you can use your email template id here
+            ->setTemplateOptions(['area' => 'frontend', 'store' => $this->storeManager->getStore()->getId()])
+            ->setTemplateVars($templateVars)
+            ->setFrom('general') // you can use your email sender setting here
+            ->addTo($email)
+            ->getTransport();
+
+        $transport->sendMessage();
     }
 }
